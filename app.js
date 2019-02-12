8dddd6499d143f4c149967628c6579a4982412b7
kauz <kauzlein@gmail.com>
2019-02-12 10:31:41 +0300

    [ADD] cors

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let cors = require('cors');
 
 
 let app = express();
 let options = require('./knexfile').development;
 let knex = require('knex')(options);
-require('./controllers')(app ,knex);
 
+app.use(cors({origin: 'http://localhost:3001'}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+require('./controllers')(app ,knex);
 
 module.exports = app;
